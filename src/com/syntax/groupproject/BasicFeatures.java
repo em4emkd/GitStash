d24f2e7ca9ec8a6a7134903eeb4dcc53725d1561
emi <em4emkd@gmail.com>
2022-05-15 11:03:20 -0400

    the basic structure of the car is ready, working on advanced features

---
+++ src/com/syntax/groupproject/BasicFeatures.java
@@ -0,0 +1,10 @@
+package com.syntax.groupproject;
+
+public class BasicFeatures {
+
+    String engineCapacity;
+    String color;
+    String doors;
+    String headLights;
+
+}
---
+++ src/com/syntax/groupproject/Main.java
@@ -0,0 +1,10 @@
+package com.syntax.groupproject;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        BasicFeatures car = new BasicFeatures();
+        
+    }
+}
