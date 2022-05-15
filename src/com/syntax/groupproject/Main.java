9e680d944db3bd1e1d5f152cb5a70f2f0b9c8f43
emi <em4emkd@gmail.com>
2022-05-15 12:05:17 -0400

    The red car is ready with some basic features

--- src/com/syntax/groupproject/Main.java
+++ src/com/syntax/groupproject/Main.java
@@ -4,7 +4,11 @@ public class Main {
 
     public static void main(String[] args) {
 
-        BasicFeatures car = new BasicFeatures();
-        
+        BasicFeatures audi = new BasicFeatures();
+        audi.color = "red";
+        audi.doors = "4";
+        audi.engineCapacity = "V8";
+        audi.headLights="Led";
+
     }
 }
